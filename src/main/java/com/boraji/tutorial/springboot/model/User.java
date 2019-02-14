90463babfa51605478422b97d11d140649a03d93
Darbetigh <mustafaakgul@yandex.com>
2019-02-14 14:41:01 +0300

    Initial commit

---
+++ src/main/java/com/boraji/tutorial/springboot/model/User.java
@@ -0,0 +1,63 @@
+package com.boraji.tutorial.springboot.model;
+
+import java.util.Date;
+
+public class User {
+
+    private String username;
+    private String password;
+    private String email;
+    private Date birthDate;
+    private String profession;
+
+    public User() {
+    }
+
+    public User(String username, String password, String email, Date birthDate, String profession) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.birthDate = birthDate;
+        this.profession = profession;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+}
